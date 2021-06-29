e38853e15dd7786e095a9236b66462667608ca70
Amanda Nelson <amanda.gaylord@gmail.com>
2021-06-28 21:53:18 -0400

    adding express.set to main index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const server = require("./api/server");
 const port = process.env.PORT;
 
+express.set("port", port);
 server.use(express.static(path.join(__dirname, "client/dist")));
 server.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/dist", "index.html"));
